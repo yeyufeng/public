38b6fbfa638b7d46e8d96f1675757a8842e5603d
yeyufeng <yeyufeng@aliyun.com>
2015-12-31 16:42:08 +0800

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by yeyufeng on 15/12/31.
+//  Copyright © 2015年 REFORMER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
